209d7043f63d0850c8c04fc238d67ca27676fd94
Dan Grund <hello@dangrund.com>
2017-02-09 09:42:25 -0700

    Update index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  // beforeModel(){
-  //   this.replaceWith('/reminders')
-  // }
   model (){
     return this.get('store').findAll('reminder')
   }

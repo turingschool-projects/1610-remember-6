4a3bf89c5d5b22b1a6e961f8e77d97bbb3e339de
Elijah Williams <ejwill04@gmail.com>
2017-02-13 12:45:16 -0700

    Remote new-test unit test and modify acceptance test to be more reminder like

--- tests/acceptance/new-note-test.js
+++ tests/acceptance/new-note-test.js
@@ -5,13 +5,13 @@ moduleForAcceptance('Acceptance | new note')
 
   test('should add a new note on submit with valid input', function(assert) {
     visit('reminders/new');
-    fillIn('.spec-input-title', 'Banana');
-    fillIn('.spec-input-date', 'One Bunch');
-    fillIn('.spec-textarea-notes', 'Only extra green ones');
+    fillIn('.spec-input-title', 'Eat a Banana');
+    fillIn('.spec-input-date', 'Now');
+    fillIn('.spec-textarea-notes', 'Only eat the extra green ones');
     click('.add-notes--submit');
     andThen(function() {
       assert.equal(currentURL(), 'reminders/new', 'should route to reminders/new');
       assert.equal(find('.spec-reminder-item').length, 1, 'should show 1 notes');
-      assert.equal(find('.spec-reminder-item').text().trim(), 'Banana', 'should list note title');
+      assert.equal(find('.spec-reminder-item').text().trim(), 'Eat a Banana', 'should list note title');
     });
   });

27ba56bdbaae40718598e3f88c22e2b0b08a0f79
DanGrund <hello@dangrund.com>
2017-02-14 15:17:22 -0700

    update input fields to containexisting info

--- app/controllers/reminders/reminder/edit.js
+++ app/controllers/reminders/reminder/edit.js
@@ -1,22 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  store: Ember.inject.service(),
-
-    title: '',
-    date: '',
-    notes: '',
 
   actions: {
     updateNote(id) {
-      const reminderProps = this.getProperties('title', 'date', 'notes');
       this.get('store').findRecord('reminder', id).then(function(reminder) {
-        reminder.set('title', reminderProps.title)
-        reminder.set('date', reminderProps.date)
-        reminder.set('notes', reminderProps.notes)
+        reminder.set('title', reminder.get('title'))
+        reminder.set('date', reminder.get('date'))
+        reminder.set('notes', reminder.get('notes'))
         reminder.save();
-      }).then(() => {
-        this.setProperties({title: '', date: '' , notes: ''})
       }).then(()=>{
         this.transitionToRoute('/reminders/' + this.model.id)
       })

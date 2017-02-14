755483728ffb96cf8521ebfb4c6eb0ce264ddef0
DanGrund <hello@dangrund.com>
2017-02-14 15:28:59 -0700

    add undo button that Rolls back changes

--- app/controllers/reminders/reminder/edit.js
+++ app/controllers/reminders/reminder/edit.js
@@ -4,13 +4,19 @@ export default Ember.Controller.extend({
 
   actions: {
     updateNote(id) {
-      this.get('store').findRecord('reminder', id).then(function(reminder) {
+      this.get('store').findRecord('reminder', id).then((reminder)=> {
         reminder.set('title', reminder.get('title'))
         reminder.set('date', reminder.get('date'))
         reminder.set('notes', reminder.get('notes'))
         reminder.save();
       }).then(()=>{
         this.transitionToRoute('/reminders/' + this.model.id)
+      })
+    },
+
+    undoEdit(id) {
+      this.get('store').findRecord('reminder', id).then((reminder)=> {
+        reminder.rollbackAttributes();
       })
     }
   }
